af74bf9fb739833514fe435e0f9a4e8129fde902
koriym <akihito.koriyama@gmail.com>
2012-02-08 19:48:40 +0900

    Remove _once.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 // bootstrap for test
-require '/Users/akihito/git/Ray.Di/vendor/Ray.Aop/vendor/Doctrine.Common/lib/Doctrine/Common/Annotations/Reader.php';
 require dirname(__DIR__) . '/vendor/Doctrine.Common/lib/Doctrine/Common/ClassLoader.php';
 $commonLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', dirname(__DIR__) . '/vendor/Doctrine.Common/lib');
 $commonLoader->register();
--- tests/src.php
+++ tests/src.php
@@ -1,4 +1,8 @@
 <?php
+require_once dirname(__DIR__) . '/vendor/Doctrine.Common/lib/Doctrine/Common/ClassLoader.php';
+$commonLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', dirname(__DIR__) . '/vendor/Doctrine.Common/lib');
+$commonLoader->register();
+
 require __DIR__ . '/MockMethod.php';
 require __DIR__ . '/MockMethodInterceptor.php';
 require __DIR__ . '/interceptors/DoubleInterceptor.php';
