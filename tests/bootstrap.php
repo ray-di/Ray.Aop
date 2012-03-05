e9fe66c0e8e0c3f70d3adb6d1e5a0857f51de159
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-03-05 19:40:00 +0900

    Src layout.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
-// bootstrap for test
-require dirname(__DIR__) . '/vendor/Doctrine.Common/lib/Doctrine/Common/ClassLoader.php';
-$commonLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', dirname(__DIR__) . '/vendor/Doctrine.Common/lib');
-$commonLoader->register();
 
+require dirname(__DIR__) . '/vendor/src.php';
 require dirname(__DIR__) . '/src.php';
 require __DIR__ . '/src.php';
