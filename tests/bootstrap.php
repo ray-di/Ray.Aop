b0dc722705666207ca880dc9715f902022041324
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-05-14 22:12:31 +0900

    Compatible with composer 1.0.3

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,4 @@ require dirname(__DIR__) . '/src.php';
 // tests
 require __DIR__ . '/src.php';
 // vendor
-require dirname(__DIR__) . '/vendor/.composer/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
