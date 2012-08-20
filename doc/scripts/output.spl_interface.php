d5bccf108974fd5552032e26c52fc190c8aa65ef
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-08-20 12:58:46 +0900

    Add comment.

--- doc/scripts/output.spl_interface.php
+++ doc/scripts/output.spl_interface.php
@@ -1,4 +1,5 @@
 <?php
+// show spl methods (which are invalid to bind)
 $methods = (new ReflectionClass('ArrayObject'))->getMethods();
 foreach ($methods as $method) {
     $result[] = $method->name;
