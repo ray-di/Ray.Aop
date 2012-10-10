eb97f0e85b1ade19faadf1c5d77ab414fd282f88
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-11 02:51:14 +0900

    More ignores.

---
+++ src/Ray/Aop/BindInterface.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: akihito
+ * Date: 2012/10/11
+ * Time: 2:18
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ src/Ray/Aop/Exception/Exception.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: akihito
+ * Date: 2012/10/11
+ * Time: 0:15
+ * To change this template use File | Settings | File Templates.
+ */
+interface Exception
+{
+
+}
