60907f06ed043c4c3277e2e8f039322b427fc673
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-16 12:23:58 +0900

    Comment.

--- src/Ray/Aop/Weave.php
+++ src/Ray/Aop/Weave.php
@@ -37,7 +37,7 @@ interface Weave
      * @param array  $params
      *
      * @return mixed
-     * @throws BadFunctionCallException
+     * @throws \BadFunctionCallException
      */
     public function  __call($method, array $params);
 
