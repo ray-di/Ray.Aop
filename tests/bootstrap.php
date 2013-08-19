cfcf1b31233bb90d7189c4e5d6022f9f7f1832f7
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-08-20 02:55:16 +0900

    use autoloader for test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,13 @@
 <?php
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
 // library
 require dirname(__DIR__) . '/src.php';
 // tests
 require __DIR__ . '/src.php';
 // vendor
-require dirname(__DIR__) . '/vendor/autoload.php';
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+/** @var $loader \Composer\Autoload\ClassLoader */
+$loader->add('Ray\Aop', [__DIR__]);
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
