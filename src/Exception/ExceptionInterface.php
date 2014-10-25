3ada219e584582322f9fa7071ed1c8c7f0f5034e
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-10-25 10:19:03 +0900

    add Exception

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the Ray.Aop package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace Ray\Aop\Exception;
+
+/**
+ * Generic exception
+ */
+interface ExceptionInterface
+{
+}
