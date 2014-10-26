7900924bea5bc08cd07788eae53412f2c5842171
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-10-26 10:04:40 +0900

    constructor for own matcher

--- src/AbstractMatcher.php
+++ src/AbstractMatcher.php
@@ -13,12 +13,30 @@ abstract class AbstractMatcher
      */
     protected $arguments = [];
 
+    public function __construct()
+    {
+        $this->arguments = func_get_args();
+    }
+
+    /**
+     * @param \ReflectionClass $class
+     * @param array            $arguments
+     *
+     * @return bool
+     */
     abstract public function matchesClass(\ReflectionClass $class, array $arguments);
 
+    /**
+     * @param \ReflectionMethod $method
+     * @param array             $arguments
+     *
+     * @return bool
+     */
     abstract public function matchesMethod(\ReflectionMethod $method, array $arguments);
 
     /**
      * Return matching condition arguments
+     *
      * @return array
      */
     public function getArguments()
