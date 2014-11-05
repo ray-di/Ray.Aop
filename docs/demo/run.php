5c0b23f6a99bf726922e796787fdd0d7c9d71aa8
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-11-05 11:31:30 +0900

    add demo/run.php

---
+++ docs/demo/run.php
@@ -0,0 +1,6 @@
+<?php
+
+passthru('php ' . __DIR__ . '/01-implicit-bind.php');
+passthru('php ' . __DIR__ . '/02-matcher-bind.php');
+passthru('php ' . __DIR__ . '/03-annotation-bind.php');
+passthru('php ' . __DIR__ . '/04-my-matcher.php');
