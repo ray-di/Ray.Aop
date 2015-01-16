4c52a69473337965522a149eb61a455ec824e461
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-01-16 13:44:05 +0900

    fix autoload in demo

--- docs/demo/bootstrap.php
+++ docs/demo/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 
-$loader = require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
-/** @var $loader \Composer\Autoload\ClassLoader */
-$loader->addPsr4('Ray\Aop\Demo\\', __DIR__ . '/src/');
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
+/** @var $loader \Composer\Autoload\ClassLoader */
+$loader = require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+$loader->addPsr4('Ray\Aop\Demo\\', __DIR__ . '/src/');
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+// tmp dir
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
