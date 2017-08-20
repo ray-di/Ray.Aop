c65d15ab07467a850d4cf88706db3f4d433e55ea
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-20 13:18:42 +0900

    exclude php7.1 only test

---
+++ tests/CodeGenPhp71Test.php
@@ -0,0 +1,48 @@
+<?php
+namespace Ray\Aop;
+
+use PhpParser\BuilderFactory;
+use PhpParser\PrettyPrinter\Standard;
+use PHPUnit\Framework\TestCase;
+
+class CodeGenPhp71Test extends TestCase
+{
+    /**
+     * @var CodeGen
+     */
+    private $codeGen;
+
+    public function setUp()
+    {
+        $this->codeGen = new CodeGen((new ParserFactory)->newInstance(), new BuilderFactory, new Standard);
+    }
+
+    public function testReturnTypeVoid()
+    {
+        $bind = new Bind;
+        $bind->bindInterceptors('returnTypeVoid', []);
+        $code = $this->codeGen->generate('a', new \ReflectionClass(FakePhp71ReturnTypeClass::class), $bind);
+        $expected = 'function returnTypeVoid() : void';
+        $this->assertContains($expected, $code);
+    }
+
+    public function testReturnTypeNullable()
+    {
+        $bind = new Bind;
+        $bind->bindInterceptors('returnNullable', []);
+        $code = $this->codeGen->generate('a', new \ReflectionClass(FakePhp71ReturnTypeClass::class), $bind);
+        $expected = 'function returnNullable(string $str) : ?';
+        $this->assertContains($expected, $code);
+
+        return $code;
+    }
+
+    /**
+     * @depends testReturnTypeNullable
+     */
+    public function testContainsStatement(string $code)
+    {
+        $this->assertContains("declare (strict_types=1);\n", $code);
+        $this->assertContains("use Composer\Autoload;\n", $code);
+    }
+}
