9b9667cc9e081147dd1b316eba8da51869836301
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-02 17:12:16 +0900

    declare strict_types

--- src/Exception/NotWritableException.php
+++ src/Exception/NotWritableException.php
@@ -1,6 +1,8 @@
 <?php
+
+declare(strict_types=1);
 /**
- * This file is part of the Ray.Aop package
+ * This file is part of the Ray.Aop package.
  *
  * @license http://opensource.org/licenses/MIT MIT
  */
