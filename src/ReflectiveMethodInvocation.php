711957136e0ade21173db3bd18caadc5996de81e
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-04-27 05:17:37 +0900

    replace ReflectionMethod to call_user_func_array

--- src/ReflectiveMethodInvocation.php
+++ src/ReflectiveMethodInvocation.php
@@ -93,7 +93,7 @@ final class ReflectiveMethodInvocation implements MethodInvocation
     public function proceed()
     {
         if ($this->interceptors === []) {
-            return (new ReflectionMethod($this->object, $this->method))->invokeArgs($this->object, $this->arguments->getArrayCopy());
+            return call_user_func_array([$this->object, $this->method], $this->arguments->getArrayCopy());
         }
         $interceptor = array_shift($this->interceptors);
 
