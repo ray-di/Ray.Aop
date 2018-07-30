a9c3630f766ff133ed1dd8ad50dbf15425b5319c
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-07-30 11:39:13 +0900

    fix cs

--- demo/run.php
+++ demo/run.php
@@ -6,7 +6,6 @@ declare(strict_types=1);
  *
  * @license http://opensource.org/licenses/MIT MIT
  */
-
 passthru('php ' . __DIR__ . '/01-explicit-bind.php');
 passthru('php ' . __DIR__ . '/02-matcher-bind.php');
 passthru('php ' . __DIR__ . '/03-annotation-bind.php');
