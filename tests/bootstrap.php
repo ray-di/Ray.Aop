c2bd57b82e0deef934f3024ea28ae2866ea2a43c
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-05-20 00:20:52 +0900

    soothe phpstan

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
-array_map('unlink', (array) glob("{$_ENV['TMP_DIR']}/*.php"));
+array_map('unlink', glob("{$_ENV['TMP_DIR']}/*.php"));
