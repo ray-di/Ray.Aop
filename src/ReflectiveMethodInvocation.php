e672f8a25e8d36c18f9bb016a38246586c4c9bef
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-04-27 02:32:50 +0900

    soothe phpstan

--- src/ReflectiveMethodInvocation.php
+++ src/ReflectiveMethodInvocation.php
@@ -12,7 +12,7 @@ final class ReflectiveMethodInvocation implements MethodInvocation
     private $object;
 
     /**
-     * @var array|\ArrayObject
+     * @var array|\ArrayObject<string, mixed>
      */
     private $arguments;
 
