111bc7110461a3433471acddf5d2509a7a6f8199
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-05-30 12:46:31 +0900

    fix cs

--- src/BindInterface.php
+++ src/BindInterface.php
@@ -26,7 +26,7 @@ interface BindInterface
      *
      * [$methodNameA => [$interceptorA, ...][]
      *
-     * @return array<string, array<string|MethodInterceptor>>
+     * @return array<string, array<MethodInterceptor|string>>
      */
     public function getBindings();
 
