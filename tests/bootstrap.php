125d80bbeb8a7e418a6bebef447706d4ab2403df
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-09-28 12:30:53 +0900

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,4 @@
 declare(strict_types=1);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
-array_map('unlink', (array) glob(__DIR__ . '/tmp/*.php'));
+array_map('unlink', array_filter((array) glob(__DIR__ . '/tmp/*.php')));
