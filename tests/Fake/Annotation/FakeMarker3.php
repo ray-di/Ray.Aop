1d61bf87039256b4b08d09ecd5b26493cb5c2a33
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-12-17 22:23:34 +0900

    Attriutes compatible annotations

--- tests/Fake/Annotation/FakeMarker3.php
+++ tests/Fake/Annotation/FakeMarker3.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Ray\Aop\Annotation;
 
+use Attribute;
+
 /**
  * @Annotation
  * @Target("METHOD")
  */
+#[Attribute(Attribute::TARGET_METHOD)]
 final class FakeMarker3
 {
 }
