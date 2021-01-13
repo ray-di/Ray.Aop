8dbab69740cfd982e3ef95632a34f25e70ec8d33
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-01-13 09:31:47 +0900

    Extract AopClass
    soothe phpmd

---
+++ src/AopClass.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ray\Aop;
+
+use PhpParser\BuilderFactory;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Parser;
+use ReflectionClass;
+
+use function array_merge;
+use function assert;
+
+final class AopClass
+{
+    /** @var CodeGenMethod */
+    private $codeGenMethod;
+
+    /** @var AopClassName */
+    private $aopClassName;
+
+    /** @var AopProps */
+    private $aopProps;
+
+    public function __construct(
+        Parser $parser,
+        BuilderFactory $factory,
+        AopClassName $aopClassName
+    ) {
+        $this->aopClassName = $aopClassName;
+        $this->codeGenMethod = new CodeGenMethod($parser);
+        $this->aopProps = new AopProps($factory);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param ReflectionClass<object> $sourceClass
+     */
+    public function __invoke(CodeVisitor $visitor, ReflectionClass $sourceClass, BindInterface $bind): Class_
+    {
+        assert($visitor->class instanceof Class_);
+        $methods = $this->codeGenMethod->getMethods($bind, $visitor);
+        $propStms = ($this->aopProps)($sourceClass);
+        $classStm = $visitor->class;
+        $newClassName = ($this->aopClassName)((string) $visitor->class->name, $bind->toString(''));
+        $classStm->name = new Identifier($newClassName);
+        $classStm->extends = new Name('\\' . $sourceClass->name);
+        $classStm->implements[] = new Name('WeavedInterface');
+        /** @var array<int, Stmt> $stmts */
+        $stmts = array_merge($propStms, $methods);
+        $classStm->stmts = $stmts;
+
+        return $classStm;
+    }
+}
--- src/CodeGen.php
+++ src/CodeGen.php
@@ -5,15 +5,10 @@ declare(strict_types=1);
 namespace Ray\Aop;
 
 use PhpParser\BuilderFactory;
-use PhpParser\Node\Identifier;
-use PhpParser\Node\Name;
-use PhpParser\Node\Stmt;
-use PhpParser\Node\Stmt\Class_;
 use PhpParser\Parser;
 use ReflectionClass;
 
 use function array_merge;
-use function assert;
 use function implode;
 
 final class CodeGen implements CodeGenInterface
@@ -21,17 +16,11 @@ final class CodeGen implements CodeGenInterface
     /** @var BuilderFactory */
     private $factory;
 
-    /** @var CodeGenMethod */
-    private $codeGenMethod;
-
-    /** @var AopClassName */
-    private $aopClassName;
-
     /** @var VisitorFactory */
     private $visitoryFactory;
 
-    /** @var AopProps */
-    private $aopProps;
+    /** @var AopClass  */
+    private $aopClass;
 
     public function __construct(
         Parser $parser,
@@ -39,10 +28,8 @@ final class CodeGen implements CodeGenInterface
         AopClassName $aopClassName
     ) {
         $this->factory = $factory;
-        $this->codeGenMethod = new CodeGenMethod($parser);
-        $this->aopClassName = $aopClassName;
         $this->visitoryFactory = new VisitorFactory($parser);
-        $this->aopProps = new AopProps($factory);
+        $this->aopClass = new AopClass($parser, $factory, $aopClassName);
     }
 
     /**
@@ -53,17 +40,7 @@ final class CodeGen implements CodeGenInterface
     public function generate(ReflectionClass $sourceClass, BindInterface $bind): Code
     {
         $visitor = ($this->visitoryFactory)($sourceClass);
-        assert($visitor->class instanceof Class_);
-        $methods = $this->codeGenMethod->getMethods($bind, $visitor);
-        $propStms = ($this->aopProps)($sourceClass);
-        $classStm = $visitor->class;
-        $newClassName = ($this->aopClassName)((string) $visitor->class->name, $bind->toString(''));
-        $classStm->name = new Identifier($newClassName);
-        $classStm->extends = new Name('\\' . $sourceClass->name);
-        $classStm->implements[] = new Name('WeavedInterface');
-        /** @var array<int, Stmt> $stmts */
-        $stmts = array_merge($propStms, $methods);
-        $classStm->stmts = $stmts;
+        $classStm = ($this->aopClass)($visitor, $sourceClass, $bind);
         $ns = $this->getNamespace($visitor);
         $stmt = $this->factory->namespace($ns)
             ->addStmt($this->factory->use(WeavedInterface::class))
