5f773a834c7a182a9d9e55c49cd0c73892c5eeda
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-07-17 19:02:56 +0900

    Get annotation reader from service location

--- tests/AnnotatedMatcherTest.php
+++ tests/AnnotatedMatcherTest.php
@@ -5,18 +5,15 @@ declare(strict_types=1);
 namespace Ray\Aop;
 
 use PHPUnit\Framework\TestCase;
-use ReflectionException;
 
 use function serialize;
 use function unserialize;
 
 class AnnotatedMatcherTest extends TestCase
 {
-    /**
-     * @throws ReflectionException
-     */
-    public function testSerilize(): void
+    public function testSerialize(): void
     {
-        $this->assertInstanceOf(AnnotatedMatcher::class, unserialize(serialize(new AnnotatedMatcher('startsWith', ['a']))));
+        $object = new AnnotatedMatcher('startsWith', ['a']);
+        $this->assertInstanceOf(AnnotatedMatcher::class, unserialize(serialize($object)));
     }
 }
