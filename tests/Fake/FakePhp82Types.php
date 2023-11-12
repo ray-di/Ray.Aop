6a6c17ac974b98d92e2191a797e497a1d5f758b2
Akihito Koriyama <akihito.koriyama@gmail.com>
2023-11-13 02:18:59 +0900

    fixup! Support PHP 8.2 DNF type

--- tests/Fake/FakePhp82Types.php
+++ tests/Fake/FakePhp82Types.php
@@ -11,15 +11,9 @@ class FakePhp82Types
 {
     public function method100(): false { return false; }
     public function method101(): true { return true; }
-
-    // Methods with scalar type declarations
     public function method102(): null { return null; }
-    // A method with no type declaration
     public function method103(): FakeNullInterface&FakeNullInterface1 { return $this;}
-
-    // A method with no type declaration
     public function method104(): FakeNullInterface|FakeNullInterface1 { return $this;}
-
     public function method105(): FakeNullInterface|string { return $this;}
     public function method106(): (FakeNullInterface&FakeNullInterface1)|string { return $this;}
 }
