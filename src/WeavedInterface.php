6872b3044f11392b68e54ef0e9b3b2b7ff979209
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-02-14 17:05:03 +0900

    Revert 2.17.1 interface
    The `_initState` method and its associated annotations were removed as they are no longer used. This simplifies the interface and reduces unnecessary code clutter.

--- src/WeavedInterface.php
+++ src/WeavedInterface.php
@@ -4,13 +4,6 @@ declare(strict_types=1);
 
 namespace Ray\Aop;
 
-/** @psalm-import-type MethodBindings from Types */
 interface WeavedInterface
 {
-    /**
-     * @param MethodBindings $bindings
-     *
-     * @SuppressWarnings(PHPMD.CamelCaseMethodName)
-     */
-    public function _initState(array $bindings): void; // phpcs:ignore
 }
